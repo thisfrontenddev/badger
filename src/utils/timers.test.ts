85521e8367238c343d89a9a3de136147e3a3377e
eric.martin <eric.martin@hey.com>
2022-07-02 19:43:41 +0200

    test(timers): add test suite for timers

---
+++ src/utils/timers.test.ts
@@ -0,0 +1,26 @@
+import { createTimer, TimerType } from "./timers";
+
+jest.useFakeTimers();
+jest.spyOn(global, "setInterval");
+
+const mockCallback = jest.fn();
+
+describe("createTimer", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  });
+
+  test("should create and launch a timer", () => {
+    const [type, timer] = createTimer({
+      name: TimerType.EYES,
+      duration: 8000,
+      callback: mockCallback,
+    });
+
+    expect(type).toBe(TimerType.EYES);
+    expect(setInterval).toHaveBeenCalledTimes(1);
+    expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 8000);
+
+    clearInterval(timer);
+  });
+});
